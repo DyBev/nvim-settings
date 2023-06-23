a46faa2314711ee6a63abd29d1c633073f685b86
DyBev <54762882+DyBev@users.noreply.github.com>
2023-06-23 14:38:37 +0100

    Update nvim settings to work on MacOS and Linux

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -9,7 +9,7 @@ parser_config.GuidedTrack = {
 
 require'nvim-treesitter.configs'.setup {
 	-- A list of parser names, or "all"
-	ensure_installed = {"javascript", "typescript", "help", "c", "lua", "rust" },
+	ensure_installed = {"javascript", "typescript", "c", "lua", "rust" },
 
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
